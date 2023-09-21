6d06d040ddcf1453f4b910499afbbbcc72e08f93
Nguyen Nhat Huy <huy.nguyen@klabs.vn>
2023-09-21 16:28:35 +0700

    Initial Commit

---
+++ GroceriesShop/GroceriesShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesShopApp.swift
+//  GroceriesShop
+//
+//  Created by Klabs Mobile on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
